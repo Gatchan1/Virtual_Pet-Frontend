cf095047783e1fa671503db332d016b105a2a3da
Gatchan1 <r.barrio.f@gmail.com>
2025-01-04 16:34:05 +0100

    test backend connection

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ import AuthenticationPage from "./pages/AuthenticationPage";
 import Layout from "./layouts/Layout";
 import { useEffect, useState } from "react";
 import AdminDashboard from "./pages/AdminDashboard";
+import { API_URL } from "./api/apiConstants";
+
+async function getTestRoute() {
+  try {
+    const data = await fetch(API_URL);
+    console.log("dataa!", data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 
 function App() {
   const { isAuthenticated, setIsAuthenticated, isLoading } = useAuth();
@@ -12,6 +23,8 @@ function App() {
   const [loadingSpinner, setLoadingSpinner] = useState(true);
 
   useEffect(() => {
+    getTestRoute();
+
     if (!isLoading)
       setTimeout(() => {
         setLoadingSpinner(false);
