c82f65d6de76405d06e4c5dceb55045f89f46861
Gatchan1 <r.barrio.f@gmail.com>
2025-01-04 13:19:14 +0100

    change api url

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://virtual-pet-094c815eee1c.herokuapp.com"; //http://localhost:8080
 
 export const petAccessories = ["HAT", "SUNGLASSES"];
 
